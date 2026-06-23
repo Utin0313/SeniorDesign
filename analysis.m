e2c8437ab519e25465a5b37503a1b0201fa6cba6
Utin0313 <trinhthanhdat2003@gmail.com>
2026-06-23 02:04:55 -0400

    Refactor analysis.m to improve code readability

--- analysis.m
+++ analysis.m
@@ -1,23 +1,19 @@
-clc; clear; close all
+clc; clear; close all 
+ 
+% -- Load data -- 
+data = readtable('prediction_n18.csv');  
+classes = ["breast", "control", "prostate", "skin"];  
+ 
+% -- Temp variable --  
+mean_conf = zeros(size(classes));
 
-% -- Load data --
-data = readtable('prediction_n18.csv'); 
-classes = ["breast", "control", "prostate", "skin"]; 
-
-% -- Temp variable -- 
-mean_conf = zeros(size(classes)); 
-
-for i=1:length(classes)
-    
-    k = classes(i); 
-
-    idx = strcmp(data.real, k);
-
-    mean_conf(i) = mean(data{idx, k});
-
+for i=1:length(classes) 
+    k = classes(i);  
+    idx = strcmp(data.real, k); 
+    mean_conf(i) = mean(data{idx, k}); 
 end 
 
-bar(categorical(classes), mean_conf)
-ylabel('Mean True-Class Probability')
-xlabel('True Class')
-ylim([0 1])
+bar(categorical(classes), mean_conf) 
+ylabel('Mean True-Class Probability') 
+xlabel('True Class') 
+ylim([0 1]) 
